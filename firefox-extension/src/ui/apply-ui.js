d6957cd541097acb7ed766be2c2c2ba180b8477f
Amir <amirmuhammadshk@gmail.com>
2025-11-10 10:55:24 +0000

    pre-refactor

--- firefox-extension/src/ui/apply-ui.js
+++
@@ -1,9 +0,0 @@
-// path: src/ui/apply-ui.js
-import { toggle } from "../utils/dom.js";
-
-export function setGenerateUIByState(app) {
-  const { generateStep, genDone } = app.el;
-  const show = ["generated"].includes(app.data.state);
-  toggle(generateStep, show);
-  genDone.classList.toggle("hidden", !(app.data.state === "generated" && app.data.generatedPrompt));
-}
